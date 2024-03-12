17005aa6784c5f3b0ada69ffb8ad966fd8807552
raoufsezar <raoufsato999@gmail,com>
2024-03-12 14:39:57 +0300

    add product detail and product list

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,7 +1,7 @@
 import { deleteUser } from "@/app/lib/actions";
 import { fetchUsers } from "@/app/lib/data";
-// import Pagination from "@/app/ui/dashboard/pagination/pagination";
-// import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/Pagination";
+import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,7 +14,7 @@ const UsersPage = async ({ searchParams }: { searchParams: any }) => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.top}>
-				{/* <Search placeholder="Search for a user..." /> */}
+				<Search placeholder="Search for a user..." />
 				<Link href="/dashboard/users/add">
 					<button className={styles.addButton}>Add New</button>
 				</Link>
@@ -68,7 +68,7 @@ const UsersPage = async ({ searchParams }: { searchParams: any }) => {
 					))}
 				</tbody>
 			</table>
-			{/* <Pagination count={count} /> */}
+			<Pagination count={count} />
 		</div>
 	);
 };
